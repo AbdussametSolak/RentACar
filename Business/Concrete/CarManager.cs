e9430278328ac68ad4fc4f988641cc6595508df7
AbdussametSolak <106724879+AbdussametSolak@users.noreply.github.com>
2022-07-31 23:45:02 +0300

    updated

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,55 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    internal class CarManager : ICarService
+    {
+        ICarDal _carDal;
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public void Add(Car car)
+        {
+            _carDal.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+        }
+
+        public Car Get(int carId)
+        {
+            return _carDal.Get(c => c.CarId == carId);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+        public List<Car> GetAllByBrandId(int brandId)
+        {
+            return _carDal.GetAll(b=>b.BrandId==brandId);
+        }
+
+        public List<Car> GetAllByColorId(int colorId)
+        {
+            return _carDal.GetAll(c=>c.ColorId==colorId);
+        }
+
+        public void Update(Car car)
+        {
+            _carDal.Update(car);
+        }
+    }
+}
